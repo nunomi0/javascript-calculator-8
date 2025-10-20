e326a589e7d3cd9db48ce092469ad2f3dc233c50
nunomi0 <ykys1@naver.com>
2025-10-20 21:47:00 +0900

    feat(app): 구분자 설정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import { Console } from '@woowacourse/mission-utils';
+import { Validator } from './Validator.js';
 
 class App {
   async run() {
     const inputString = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+
+    try {
+      const { delimiter, numbersPart } = Validator(inputString);
+      const regex = new RegExp(delimiter);
+
+    } catch (error) {
+      Console.print(error.message);
+    }
   }
 }
 
