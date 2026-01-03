11424d54522bddfc347a262aa51c58233f49e12e
nunomi0 <ykys1@naver.com>
2026-01-03 20:27:46 +0900

    feat(app): 기본구분자, 커스텀 구분자 판별

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import { Console } from '@woowacourse/mission-utils';
 class App {
   async run() {
     const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
-    // Console.print(input);
+
+    let delimeters = [",", ":"];
+
+    // 커스텀 구분자 지정
+    if (input.startsWith("//")) {
+      const end = input.indexOf("\\n");
+      delimeters = input.slice(2,end);
+    }
+    
+    Console.print(delimeters);
   }
 }
 
